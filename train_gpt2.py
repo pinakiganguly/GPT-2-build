207221da09d0b8900698080894b236f83a592696
pinakiganguly <pinakiganguly0003@gmail.com>
2026-03-18 00:44:51 +0530

    added one section for optimizer

--- train_gpt2.py
+++ train_gpt2.py
@@ -211,26 +211,26 @@ class GPT(nn.Module):
 
         return model
 
-    def configure_optimizers(self, weight_decay, learning_rate, device_type):
+    def configure_optimizers(self, weight_decay, learning_rate, device_type): 
         # start with all of the candidate parameters (that require grad)
         param_dict = {pn: p for pn, p in self.named_parameters()}
         param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
         # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
         # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
         decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
-        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2] # -- 1D tensors
         optim_groups = [
             {'params': decay_params, 'weight_decay': weight_decay},
             {'params': nodecay_params, 'weight_decay': 0.0}
-        ]
+        ]                    #from line 220 to 230 we are just reducing the weights by using the weight decay parameter, so that the training can be done with more stability. Then finally printing out those.
         num_decay_params = sum(p.numel() for p in decay_params)
         num_nodecay_params = sum(p.numel() for p in nodecay_params)
         # if master_process:
         print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
         print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
         # Create AdamW optimizer and use the fused version if it is available
-        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
-        use_fused = fused_available and device_type == "cuda"
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters  #new fused= parameter added my pytorch for AdamW optimizer
+        use_fused = fused_available and device_type == "cuda"  #fused is much faster operation that can be implemented using cuda. Instead of iterating through the for loop for all the tensors, it generates lot of kernels for computation, after the computation is done it combines all into one kernel and finally updates it with all the parameters (kernel fusion update)
         print(f"using fused AdamW: {use_fused}")
         optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
         return optimizer
